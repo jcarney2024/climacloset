1b234513bfc10388ab0bbec285008ec1cf15bbef
Jack C <j.carney@bulldogs.yale.edu>
2024-12-06 16:18:38 -0500

    Add image to DB

--- app/models.py
+++ app/models.py
@@ -6,15 +6,10 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
+    image_file = db.Column(db.String(130), nullable=False, default='default.jpg')
     name = db.Column(db.String(255))
     posts = db.relationship('Post', backref='author', lazy=True)
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
     
 @login_manager.user_loader
 def load_user(id):
