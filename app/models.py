37d82de78ddaa80d0353fbbe5b3f65039c39ba5e
Jack C <j.carney@bulldogs.yale.edu>
2024-12-06 16:04:22 -0500

    Add Posts to Database

--- app/models.py
+++ app/models.py
@@ -7,6 +7,14 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     name = db.Column(db.String(255))
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
     
 @login_manager.user_loader
 def load_user(id):
