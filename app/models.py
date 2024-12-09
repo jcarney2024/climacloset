095383c0447f4ad2dc99e0fc943d49db27f241c2
Jack C <j.carney@bulldogs.yale.edu>
2024-12-09 14:46:44 -0500

    Update user model

--- app/models.py
+++ app/models.py
@@ -6,9 +6,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
-    image_file = db.Column(db.String(130), nullable=False, default='default.jpg')
     name = db.Column(db.String(255))
-    posts = db.relationship('Post', backref='author', lazy=True)
+    image_file = db.Column(db.String(130), nullable=False, default='default.jpg')
 
     
 @login_manager.user_loader
