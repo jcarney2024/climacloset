a4c813e9094926bee31b208587899365574991ba
Jack C <j.carney@bulldogs.yale.edu>
2024-12-10 17:27:33 -0500

    Fix Edit Name and Image features

--- app/edit.py
+++ app/edit.py
@@ -3,28 +3,39 @@ from flask_login import login_required, current_user
 from .models import User
 from . import db
 
-profile_blueprint = Blueprint('profile', __name__)
+profile = Blueprint('profile', __name__)
 
 # Route to display the profile page
-@profile_blueprint.route('/')
+@profile.route('/')
 @login_required
-def profile():
+def show_profile():  # Renamed from 'profile'
     user = current_user  # Get the current logged-in user
     return render_template('profile.html', name=user.name, current_user=user)
 
 # Route to edit the user's name
-@profile_blueprint.route('/profile/edit_name', methods=['GET', 'POST'])
+@profile.route('/edit_name', methods=['POST'])
 @login_required
 def edit_name():
-    if request.method == 'POST':
-        new_name = request.form['new_name']
-        if new_name:
-            current_user.name = new_name  
-            db.session.commit()  
-            flash("Name updated successfully!", "success")
-        else:
-            flash("Name cannot be empty!", "error")
-        return redirect(url_for('profile.profile'))  
+    new_name = request.form['new_name']
+    if new_name:
+        current_user.name = new_name  
+        db.session.commit()  
+        flash("Name updated successfully!", "success")
+        return render_template('profile.html', name=new_name, current_user=current_user)
+    else:
+        flash("Name cannot be empty!", "error")
+        return redirect(url_for('profile.show_profile'))
 
-    return redirect(url_for('profile.profile')) 
-
+@profile.route('/upload_picture', methods=['POST'])
+@login_required
+def upload_picture():
+    image = request.form['profile_picture']
+    if image:
+        user = current_user
+        current_user.image_file = image
+        db.session.commit()
+        flash("Image uploaded successfully!", "success")
+        return render_template('profile.html', name=user.name, current_user=current_user)
+    else:
+        flash("No image selected!", "error")
+        return redirect(url_for('profile.show_profile'))
